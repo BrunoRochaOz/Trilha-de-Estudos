7288b021fcaa1f9fa1aff9d7ea1758e850b7f35b
Bruno <brun.rocha@hotmail.com>
2022-10-04 15:18:05 -0300

    First Commit

---
+++ Java/IntroducaoIDE/src/com/Dio/MeuPrimeiroPrograma.java
@@ -0,0 +1,29 @@
+package com.Dio;
+
+import com.Dio.model.Gato;
+
+public class MeuPrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		
+		System.out.println(gato);
+		
+		
+		
+		/*int a = 2;
+		int b = 3;
+		
+		System.out.println("Hello World!!!" + (a+b));*/
+				
+	}
+	
+class Livros {
+		private String nome;
+		private String npag;
+		
+	}
+
+}
